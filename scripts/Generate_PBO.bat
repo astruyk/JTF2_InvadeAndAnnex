fa22f37334980a690fef925e9a76a23db6864c94
Anton <Anton@.(none)>
2014-10-21 21:45:15 -0400

    Add support scripts for packing mission.

---
+++ scripts/Generate_PBO.bat
@@ -0,0 +1,3 @@
+..\lib\PBOManager_v_1_4_beta\PBOConsole.exe -pack ..\mission ..\bin\JTF2_Invade_And_Annex.Altis.pbo 
+if exist ..\bin\\JTF2_Invade_And_Annex.Altis.pbo.bak del ..\bin\\JTF2_Invade_And_Annex.Altis.pbo.bak
+pause
